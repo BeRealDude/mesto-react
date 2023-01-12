f3785aa6d1e09b9263b7bc5019f0352ecf014b11
Vladislav Nikitin <vlad.nikitin1997@bk.ru>
2023-01-13 02:25:36 +0300

    feat: add imperative handlers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {  } from 'react';
+import React from 'react';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer'
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,34 @@
 import Elements from '../Elements/Elements'
+//import { useState } from 'react';
+
 
 function Main() {
+   // const [isOpenPopup, setOpenPopup] = useState();
+
+  function handleEditProfileClick(){
+    document.querySelector('.profile-popup').classList.add('popup_opened');
+  }
+
+  function handleAddPlaceClick(){
+    document.querySelector('.elements-popup').classList.add('popup_opened');
+  }
+
+  function handleEditAvatarClick(){
+    document.querySelector('.profile-popup-avatar').classList.add('popup_opened');
+  }
     return (
         <main className="content">
         <div className="profile">
-            <div className="profile__avatar">
+            <div onClick={handleEditAvatarClick} className="profile__avatar">
                 <img className="profile__avatarImg" src="#" alt="" />
                 <div className="profile__overlay"></div>
             </div>
             <div className="profile-info">
                 <h1 className="profile-info__name">{}</h1>
                 <p className="profile-info__activity">{}</p>
-                <button type="button" className="profile-info__edit-button profile-info__open-popup" />
+                <button onClick={handleEditProfileClick} type="button" className="profile-info__edit-button profile-info__open-popup" />
             </div>
-            <button type="button" className="profile__add-button profile__open-popup" />
+            <button onClick={handleAddPlaceClick} type="button" className="profile__add-button profile__open-popup" />
         </div>
         <section className="cards">
         <ul className="elements">
